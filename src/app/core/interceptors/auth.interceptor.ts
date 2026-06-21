ee8db635112e201be71e3cd4c9ecc0dbd14b3d4c
Daniel Bianchi <daniel.bianchi07@outlook.com>
2026-06-20 21:53:47 -0300

    adjust

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -23,8 +23,8 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(clonedReq).pipe(
     catchError((error: HttpErrorResponse) => {
-      if (error.status === 401) {        
-        authService.setAuthenticated(false);        
+      if (error.status === 401) {
+        authService.setAuthenticated(false);
         if (!router.url.includes('/login')) {
           router.navigate(['/login']);
         }
